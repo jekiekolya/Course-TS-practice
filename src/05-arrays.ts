d8e5974b9e1a92f95c94aa2e968d79ee813f64ac
Mykola Berezhniuk <bermukola@gmail.com>
2022-12-30 00:12:41 +0200

    Update 05-arrays.ts

--- src/05-arrays.ts
+++ src/05-arrays.ts
@@ -4,7 +4,22 @@ const str: string[] = ['qwe', 'qwe'];
 const str123: Array<string> = ['qwe', 'qwe'];
 str.push('asd');
 
-interface Car {
+// -------------------------------------------
+interface CarNew {
   wheels: number;
   brand: string;
+}
+
+const cars: CarNew[] = [];
+cars.push({ wheels: 4, brand: 'Audi' });
+
+// --------------------------------------------------------
+const arrOfArr: number[][] = [[], []];
+arrOfArr.push([1, 2, 3]);
+
+// -----------------------------------------------------
+function logParams(arr: unknown[]): void {
+  arr.forEach(item => {
+    console.log(item);
+  });
 }

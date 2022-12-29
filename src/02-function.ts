6de90f6e08e8d6027a53ee8d5d00ed3969b368ef
Mykola Berezhniuk <bermukola@gmail.com>
2022-12-29 20:21:51 +0200

    add function

---
+++ src/02-function.ts
@@ -0,0 +1,25 @@
+function sum(a: number, b: number): number {
+  return a + b;
+}
+const sum2 = (a: number, b: number): number => {
+  return a + b;
+};
+const sum3 = function sum(a: number, b: number): number {
+  return a + b;
+};
+sum(1, 5);
+
+// ------------------------------------------------------------------------
+function log(name: string, userId?: number): void {
+  console.log(name);
+}
+log('Jo');
+
+function createError(): never {
+  throw new Error('error');
+}
+
+function average(...array: number[]): number {
+  const sum = array.reduce((acc, item) => acc + item, 0);
+  return sum / array.length;
+}

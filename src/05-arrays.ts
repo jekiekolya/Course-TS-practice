91117ca83ac0c2e88594c3877b2e3e3d7d195dab
Mykola Berezhniuk <bermukola@gmail.com>
2022-12-29 23:58:57 +0200

    Update 05-arrays.ts

---
+++ src/05-arrays.ts
@@ -0,0 +1,10 @@
+const numbers = [1, 2, 3, 4, 5];
+
+const str: string[] = ['qwe', 'qwe'];
+const str123: Array<string> = ['qwe', 'qwe'];
+str.push('asd');
+
+interface Car {
+  wheels: number;
+  brand: string;
+}

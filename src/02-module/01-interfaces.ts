1f1c89d4883ccb35e4403edd0ebc6b23126a9460
Mykola Berezhniuk <bermukola@gmail.com>
2023-01-01 16:27:31 +0200

    add interfacess

---
+++ src/02-module/01-interfaces.ts
@@ -0,0 +1,47 @@
+interface User {
+  readonly email: string;
+  readonly login: string;
+  password: string;
+}
+
+interface User {
+  isOnline?: boolean;
+}
+
+const user1: User = {
+  email: 'myEmail',
+  login: 'myLogin',
+  password: 'myPassword',
+  isOnline: false,
+};
+
+// ---------------------------Expand WINDOW------------------
+// interface Window {
+//   isAuth?: boolean;
+// }
+// window.isAuth
+
+// --------------------------Connecting interfaces--------------------
+interface Person {
+  readonly firstName: string;
+  lastName: string;
+  phone?: string;
+  yearOfBirth?: number;
+}
+
+interface Employee extends User, Person {
+  contractStart: Date;
+}
+
+interface Developer extends Employee {
+  skills: string[];
+  phone: string; // change field
+  level?: 'junior' | 'middle' | 'senior';
+  say(): void; // methods
+  code?: (arg: string) => void; // methods
+}
+
+// --------------------Implement interface to class ---------------------
+// class MyNewClass implements Developer {
+
+// }

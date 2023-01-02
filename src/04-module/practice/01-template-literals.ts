668742bf6a11ada1f48732f7194791ac430bed67
Mykola Berezhniuk <bermukola@gmail.com>
2023-01-02 20:20:35 +0200

    add typeof

---
+++ src/04-module/practice/01-template-literals.ts
@@ -0,0 +1,10 @@
+export {}
+
+type Style = 'none' | 'dotted' | 'dashed' | 'solid';
+type Color = 'Red' | 'Green' | 'Blue' | 'Black' | 'White';
+
+// border-style: 'solid
+type BorderStyle = `${Style} ${Uncapitalize<Color>}`
+
+let borderStyle: BorderStyle = 'solid red';
+let borderStyle2: BorderStyle = 'dashed white';

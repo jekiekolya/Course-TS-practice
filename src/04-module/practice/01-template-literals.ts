d1cab9e0fe6c8e12cc80a95e5feedf1cef9dc54b
Mykola Berezhniuk <bermukola@gmail.com>
2023-01-04 23:03:24 +0200

    Update 01-template-literals.ts

--- src/04-module/practice/01-template-literals.ts
+++ src/04-module/practice/01-template-literals.ts
@@ -1,10 +1,10 @@
-export {}
+export {};
 
 type Style = 'none' | 'dotted' | 'dashed' | 'solid';
 type Color = 'Red' | 'Green' | 'Blue' | 'Black' | 'White';
 
 // border-style: 'solid
-type BorderStyle = `${Style} ${Uncapitalize<Color>}`
+type BorderStyle = `${Style} ${Uncapitalize<Color>}`;
 
 let borderStyle: BorderStyle = 'solid red';
 let borderStyle2: BorderStyle = 'dashed white';

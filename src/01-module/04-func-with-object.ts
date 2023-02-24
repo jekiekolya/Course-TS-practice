1d015804073f56f01e85724cf9a2a028c644f5f0
Mykola Berezhniuk <108493349+jekiekolya@users.noreply.github.com>
2023-02-24 16:17:09 +0200

    repeat functions

--- src/01-module/04-func-with-object.ts
+++ src/01-module/04-func-with-object.ts
@@ -1,24 +1,49 @@
-function logPoint(point: { x: number; y: number }): void {
-  console.log(`Coordinate: x: ${point.x}, y: ${point.y}`);
+// function logPoint(point: { x: number; y: number }): void {
+//   console.log(`Coordinate: x: ${point.x}, y: ${point.y}`);
+// }
+
+// const obg1 = {
+//   x: 1,
+//   y: 2,
+//   z: 3,
+// };
+
+// logPoint(obg1);
+
+// // ------------------------------------------------
+
+// function printName(user: { firstName: string; lastName?: string }): void {
+//   console.log('hello:', user.firstName);
+
+//   if (user.lastName) {
+//     console.log('Welcome:', user.lastName.toUpperCase());
+//   }
+// }
+
+// printName({ firstName: 'Jack' });
+// printName({ firstName: 'Jack', lastName: 'Crocodile' });
+
+// ------------------------------ NEW -------------------------
+function printPoint(point: { x: number; y: number }): void {
+  console.log(`Coordinate: x - ${point.x} y - ${point.y}`);
 }
 
-const obg1 = {
-  x: 1,
-  y: 2,
-  z: 3,
+const objNew1 = {
+  x: 12,
+  y: 15,
+  z: 13,
 };
 
-logPoint(obg1);
+printPoint(objNew1);
 
-// ------------------------------------------------
-
-function printName(user: { firstName: string; lastName?: string }): void {
-  console.log('hello:', user.firstName);
+function printUser(user: { firstName: string; lastName?: string }): void {
+  console.log('FirstName', user.firstName.toLocaleLowerCase());
+  console.log('LastName', user.lastName?.toLocaleLowerCase());
 
   if (user.lastName) {
-    console.log('Welcome:', user.lastName.toUpperCase());
+    console.log('LastName', user.lastName.toLocaleLowerCase());
   }
 }
 
-printName({ firstName: 'Jack' });
-printName({ firstName: 'Jack', lastName: 'Crocodile' });
+printUser({ firstName: 'Jack' });
+printUser({ firstName: 'Jack', lastName: 'Crocodile' });

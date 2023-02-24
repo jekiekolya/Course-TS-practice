2366eda4856b0d899f2f5642cb5390327746b595
Mykola Berezhniuk <108493349+jekiekolya@users.noreply.github.com>
2023-02-24 20:32:01 +0200

    Update 09-enum.ts

--- src/01-module/09-enum.ts
+++ src/01-module/09-enum.ts
@@ -1,27 +1,67 @@
+// enum ShapeKind {
+//   Circle = 12, // 0
+//   Square, // 1
+// }
+
+// const myNumber = ShapeKind.Circle;
+// console.log(myNumber);
+
+// interface myInterfaceCircle {
+//   kind: ShapeKind.Circle;
+//   circle: number;
+// }
+
+// interface myInterfaceSquare {
+//   kind: ShapeKind.Square;
+//   square: number;
+// }
+
+// const MyCircle: myInterfaceCircle = {
+//   kind: 0,
+//   circle: 12,
+// };
+
+// // --------------------------------------------------
+// enum StatusCode {
+//   ERROR = 500,
+//   NOT_FOUND = 404,
+//   NOT_AUTH = 403,
+// }
+
+// enum Grades {
+//   Junior = 'junior',
+//   Middle = 'middle',
+//   Senior = 'senior',
+// }
+
+// function compareGrade(user: { level: Grades }) {
+//   console.log(user.level === Grades.Junior);
+// }
+
+//  ---------------------- new -----------------------------
+
 enum ShapeKind {
-  Circle = 12, // 0
+  Circle, // 0
   Square, // 1
 }
 
-const myNumber = ShapeKind.Circle;
-console.log(myNumber);
+const myShape = ShapeKind.Circle;
 
-interface myInterfaceCircle {
+interface Circle {
   kind: ShapeKind.Circle;
-  circle: number;
+  radius: number;
 }
 
-interface myInterfaceSquare {
+interface Square {
   kind: ShapeKind.Square;
-  square: number;
+  sideLength: number;
 }
 
-const MyCircle: myInterfaceCircle = {
-  kind: 0,
-  circle: 12,
+const circle1: Circle = {
+  kind: 2,
+  radius: 2,
 };
 
-// --------------------------------------------------
 enum StatusCode {
   ERROR = 500,
   NOT_FOUND = 404,
@@ -32,8 +72,4 @@ enum Grades {
   Junior = 'junior',
   Middle = 'middle',
   Senior = 'senior',
-}
-
-function compareGrade(user: { level: Grades }) {
-  console.log(user.level === Grades.Junior);
 }

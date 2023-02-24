d7b5869e197f46cd05ce97aa8077b7f1149d6b30
Mykola Berezhniuk <108493349+jekiekolya@users.noreply.github.com>
2023-02-24 18:51:52 +0200

    Update 06-tuples.ts

--- src/01-module/06-tuples.ts
+++ src/01-module/06-tuples.ts
@@ -1,12 +1,26 @@
+// const pairs: [string, string][] = [
+//   ['name', 'Jack'],
+//   ['lastName', 'Jon'],
+// ];
+// pairs.push(['1', '2']);
+// const data: [number, boolean, string] = [1, true, 'test'];
+
+// // --------CSV--------------------------
+// const doc: [string, string, number, Date][] = [];
+// doc.push(['jack', 'jo', 23, new Date(1999, 6, 15)]);
+
+// console.log(doc);
+
+//  -------------------- new ----------------------------
+
 const pairs: [string, string][] = [
-  ['name', 'Jack'],
-  ['lastName', 'Jon'],
+  ['str', 'str1'],
+  ['str', 'str1'],
 ];
-pairs.push(['1', '2']);
-const data: [number, boolean, string] = [1, true, 'test'];
 
-// --------CSV--------------------------
+const data: [number, string, boolean] = [123, 'asd', true];
+
+// csv
 const doc: [string, string, number, Date][] = [];
-doc.push(['jack', 'jo', 23, new Date(1999, 6, 15)]);
 
-console.log(doc);
+doc.push(['Jon', 'Seen', 24, new Date(1999, 6, 15)]);

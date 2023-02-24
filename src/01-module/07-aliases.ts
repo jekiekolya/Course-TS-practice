91ac9fee38411f0bef57927fb725c752f65839fb
Mykola Berezhniuk <108493349+jekiekolya@users.noreply.github.com>
2023-02-24 19:05:59 +0200

    Update 07-aliases.ts

--- src/01-module/07-aliases.ts
+++ src/01-module/07-aliases.ts
@@ -1,6 +1,20 @@
+// type MyBoolean = false | true;
+
+// //  [string, string][]
+// type Pair = [string, string];
+// type Pairs = Pair[];
+
+// type DiffCar = {
+//   wheels: number;
+//   brand: string;
+//   type: string;
+
+//   name?: string;
+//   [key: string]: unknown;
+// };
+
 type MyBoolean = false | true;
 
-//  [string, string][]
 type Pair = [string, string];
 type Pairs = Pair[];
 
@@ -8,7 +22,5 @@ type DiffCar = {
   wheels: number;
   brand: string;
   type: string;
-
   name?: string;
-  [key: string]: unknown;
 };
